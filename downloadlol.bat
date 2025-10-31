722794066447d65ac3b6befc5f51069ba38ed539
aaronestaaron <mmaryanti778@gmail.com>
2025-10-31 12:51:48 +0700

    Update downloadlol.bat

--- downloadlol.bat
+++ downloadlol.bat
@@ -1,7 +1,7 @@
 @echo off
 cls
 cd %userprofile%\Downloads
-curl -O https://raw.githubusercontent.com/aaronestaaron/bedt.external.download.github.io/refs/heads/main/bedt_external___.bat
+curl -O https://raw.githubusercontent.com/aaronestaaron/bedt.external.download.github.io/refs/heads/main/bedt_external.bat
 echo bedt external has downloaded.
 echo.
 echo press any key in ur keyboard to open it
